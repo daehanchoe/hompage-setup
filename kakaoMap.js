82d850335d5c04ff4af8636ba821884162c7857a
choedaehan <mydh0616@gmail.com>
2026-06-05 16:04:39 +0900

    6월 5일 반영

--- kakaoMap.js
+++ kakaoMap.js
@@ -11,9 +11,9 @@ function kakaoMap() {
 
   const positions = [
     {
-      title: '신우레미콘',
-      lat: 35.7180,
-      lng: 129.3220
+      title: '신우레미콘(주) 경주공장',
+      lat: 35.6641721,
+      lng: 129.2949612
     },
     {
       title: '신우레미콘(주) 울산공장',
@@ -22,33 +22,33 @@ function kakaoMap() {
     },
     {
       title: '영화레미콘(주) 경주공장',
-      lat: 35.8190,
-      lng: 129.2690
+      lat: 35.7315563,
+      lng: 129.2046277
     },
     {
       title: '흥진레미콘(주) 경주공장',
-      lat: 35.8300,
-      lng: 129.2300
+      lat: 35.9030449,
+      lng: 129.240271
     },
     {
-      title: '영진레미콘',
-      lat: 35.7180,
-      lng: 129.3220
+      title: '영진레미콘아스콘',
+      lat: 35.5098229,
+      lng: 129.1967271
     },
     {
       title: '우진레미콘(주) 경주공장',
-      lat: 35.8305,
-      lng: 129.2305
+      lat: 35.9029163,
+      lng: 129.239217
     },
     {
       title: '신우레미콘아스콘(주) 영천공장',
-      lat: 35.9730,
-      lng: 128.8510
+      lat: 35.9767712,
+      lng: 128.9826349
     },
     {
       title: '신아레미콘(주) 울산공장',
-      lat: 35.414900049312,
-      lng: 129.3515808527
+      lat: 35.4150735,
+      lng: 129.3544048
     },
     {
       title: '신우레미콘산업(주) 울산공장',
@@ -63,6 +63,7 @@ function kakaoMap() {
   ];
 
   const bounds = new kakao.maps.LatLngBounds();
+  let openedOverlay = null;
 
   positions.forEach((position) => {
 
@@ -76,16 +77,23 @@ function kakaoMap() {
       position: markerPosition
     });
 
-    const infowindow = new kakao.maps.InfoWindow({
+    const overlay = new kakao.maps.CustomOverlay({
+      position: markerPosition,
+      yAnchor: 2.15,
       content: `
-        <div style="padding:8px 12px;">
+        <div class="map-info-window">
           ${position.title}
         </div>
       `
     });
 
     kakao.maps.event.addListener(marker, 'click', function () {
-      infowindow.open(map, marker);
+      if (openedOverlay) {
+        openedOverlay.setMap(null);
+      }
+
+      overlay.setMap(map);
+      openedOverlay = overlay;
     });
 
     bounds.extend(markerPosition);
